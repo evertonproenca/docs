74fa3040554728e1775f6a9b03850dc1247c3014
Fabio-Sodre <fabio.soudre@hotmail.com>
2014-05-24 16:25:00 -0300

    Update Cliente.php
    Atribuindo todos os atributos do banco

--- MVC/Vo/Cliente.php
+++ MVC/Vo/Cliente.php
@@ -1,5 +1,14 @@
 <?php
 
 class Vo_Cliente extends Vo {
-    protected $atributos = array('id', 'razao_social', 'nome_fantasia', 'cnpj', 'insc_est');
+    protected $atributos = array('id','razao_social','nome_fantasia','cnpj','insc_est','data_fun','ramo_atv','endereco',
+    'bairro','cep','cidade','uf','telefone','email','socio','cargo','produto_Mven','qtde_Mens','und_Med','marg_lucro',
+    'faturamento','concorrente','vi_concorrente','ponto_forte','ponto_melhoria','promocao','meio_ut','ponto_forte',
+    'sinalizacao','espaco_prom','forma_preco','preco_conc','fat_MesAno','fat_valor','mov_incio','mov_fim','mov_dia',
+    'mov_horario','form_pag','vlr_diario','vlr_mensal','razao','uf','produto','lot_minimo','rela_forn','vlrCompra_mesAno',
+    'vlr_compra','cli_mensal','perf_classA','perf_classB','perf_classC','perf_classD','perf_idadeA','perf_idadeB',
+    'perf_idadeC','perf_idadeD','perf_masc','perf_fem','venEmp_situ','perct_fat','vpesquisa_resp','frequencia','colab',
+    'colab_treino','escolaridade','crit_colab','fat_cand','atv_mot','demi_contrat','fluxo_caixa','controle_est',
+    'contas_pagar','contas_receb','adm_compra','adm_venda','faturamento','outros','planej','recurs_terc','maior_gasto',
+    'impost','atu_fiscal','acao_judi','trat_forn','cob_emp','ass_juridica','parc_acic','parc_unipar');
 }
