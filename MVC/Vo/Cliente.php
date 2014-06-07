ca389c6599e87569c46d16cdd7f2d64762533305
Fabio-Sodre <fabio.soudre@hotmail.com>
2014-06-07 15:33:01 -0300

    Update Cliente.php

--- MVC/Vo/Cliente.php
+++ MVC/Vo/Cliente.php
@@ -1,6 +1,25 @@
 <?php
 
 class Vo_Cliente extends Vo {
-    protected $atributos = array("razao","nomefan","cnpj","inscEst","dataFun","ramoAtiv","endereco","bairro","cep","cidade"
-    ,"uf","fone","email","socio1","socio2","socio3","cargo1","cargo2","cargo3");
+    protected $atributos = array("razao","nomefan","cnpj","inscEst","dataFun","ramoAtiv","endereco","bairro","cep","cidade",
+    "uf","fone","email","socio1","socio2","socio3","cargo1","cargo2","cargo3","prodMVen1","prodMVen2","prodMVen3","qtdM1",
+    "qtdM2","qtdM3","undMed1","undMed2","undMed3","margeLuc1","margeLuc2","margeLuc3","fatura1","fatura2","fatura3",
+    "concorrente1","concorrente2","concorrent3","visitConcoS","visitConcoN","pontoAten","pontoEstac","pontoPreco",
+    "pontoVarie","pontoEspFisico","pontoLocal","pontoQuali","pontoOutros","pontoMAten","pontoMEstac","pontoMPreco",
+    "pontoMVarie","pontoMEspFisico","pontoMLocal","pontoMQuali","pontoMOutros","promo1","promo2","promo3","meio1",
+    "meio2","meio3","meio4","estSinalS","estSinalN","espPromoS","espPromoN","formPreco1","formPreco2","formPreco3",
+    "precoConc1","precoConc2","precoConc3","mesAno1","mesAno2","mesAno3","mesAno4","mesAno5","mesAno6","faturamento1",
+    "faturamento2","faturamento3","faturamento4","faturamento5","faturamento6","periodoMes1","periodoMes2","dia1","dia2",
+    "dia3","dia4","dia5","dia6","dia7","horaMov1","horaMov2","horaMov3","formPag1","formPag2","formPag3","formPag4",
+    "formPag5","formPag6","valorMedioP","valorMedioM","razaoFant1","razaoFant2","razaoFant3","cidUf1","cidUf2","cidUf3",
+    "produto1","produto2","produto3","lotMinimo1","lotMinimo2","lotMinimo3","relacionamento1","relacionamento2",
+    "relacionamento3","mesAno1","mesAno2","mesAno3","mesAno4","mesAno5","mesAno6","compra1","compra2","compra3","compra4",
+    "compra5","compra6","quantCli","classeA","classeB","classeC","classeD","idade1","idade2","idade3","idade4","masc","fem",
+    "vendEmpS","vendEmpN","pesqCli1","pesqCli2","pesqCli3","qtdColab","colabT1","colabT2","escola1","escola2","escola3",
+    "contColab1","contColab2","contColab3","fatPrior1","fatPrior2","fatPrior3","ativMotiv1","ativMotiv2","ativMotiv3",
+    "demiCont","tipoM1","tipoM2","tipoM3","tipoM4","tipoM5","tipoM6","tipoM7","tipoM8","tipoI1","tipoI2","tipoI3","tipoI4",
+    "tipoI5","tipoI6","tipoI7","tipoI8","planeja1","planeja2","recursoTer1","recursoTer2","recursoTer3","recursoTer4",
+    "recursoTer5","recursoTer6","maiorG1","maiorG2","maiorG3","maiorG4","maiorG5","maiorG6","maiorG7","maiorG8",
+    "impostoEmpS","mpostoEmpN","autuacaoFiscS","autuacaoFiscN","acaoJudicialS","acaoJudicialN","tratFor1","tratFor2",
+    "cobanEmp1","cobanEmp2","cobanEmp3","cobanEmp4","acessJuridicaS","acessJuridicaN","parcerAcic","parcerUnipar");
 }
