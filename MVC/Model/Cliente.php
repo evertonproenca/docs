c74a414e7ba086ae88c429071618d125a10b7699
Fabio-Sodre <fabio.soudre@hotmail.com>
2014-06-09 12:29:00 -0300

    Update Cliente.php

--- MVC/Model/Cliente.php
+++ MVC/Model/Cliente.php
@@ -79,13 +79,77 @@ return true;
 }
     }
     
-    if (!empty($_POST['concorrente'])) {
+    if (!empty($_POST['concorrente1'] && ['concorrente2']&&['concorrente3'])) {
 return true;
 } else {
 	echo 'O campo Concorrente precisa ser preenchido pelo menos uma vez';
 	return false;
 }
+
+if (!empty($_POST['pontoAten'] && ['pontoEstac']&&['pontoPreco']&&['pontoVarie']&&['pontoEspFisico']&&['pontoLocal']
+&&['pontoQuali']&&['pontoOutros'])) {
+return true;
+} else {
+	echo 'Pelo menos um opção tem que ser selecionada! (2.4 Pontos Fortes)';
+	return false;
+}
+
+if (!empty($_POST['pontoMAten'] && ['pontoMEstac']&&['pontoMPreco']&&['pontoMVarie']&&['pontoMEspFisico']&&['pontoMLocal']
+&&['pontoMQuali']&&['pontoMOutros'])) {
+return true;
+} else {
+	echo 'Pelo menos um opção tem que ser selecionada! (2.5 Pontos Melhoria)';
+	return false;
+}
+
+
+
+if (!empty($_POST['promo1'] && ['promo2']&&['promo3'])) {
+return true;
+} else {
+	echo 'Pelo menos um opção tem que ser selecionada! (2.6 Promoção)';
+	return false;
+}
+
+if (!empty($_POST['meio1'] && ['meio2']&&['meio3']&&['meio4'])) {
+return true;
+} else {
+	echo 'Pelo menos um opção tem que ser selecionada! (2.7 Meios Utilizados)';
+	return false;
+}
+
+if (!empty($_POST['formPreco1'] && ['formPreco2']&&['formPreco3'])) {
+return true;
+} else {
+	echo 'Pelo menos um opção tem que ser selecionada! (3.1 Como forma preço dos Produtos)';
+	return false;
+}
+
+if (!empty($_POST['precoConc1'] && ['precoConc2']&&['precoConc3'])) {
+return true;
+} else {
+	echo 'Pelo menos um opção tem que ser selecionada! (3.2 Preços em relação a Concorrencia)';
+	return false;
+}
+
+if (!empty($_POST['dia1'] && ['dia2']&&['dia3']&&['dia4']&&['dia5']&&['dia6']&&['dia7'])) {
+return true;
+} else {
+	echo 'Pelo menos um opção tem que ser selecionada! (4.2 Dias de maior Movimento)';
+	return false;
+}
+
+if (!empty($_POST['horaMov1'] && ['horaMov2']&&['horaMov3'])) {
+return true;
+} else {
+	echo 'Pelo menos um opção tem que ser selecionada! (4.3 Horario de Maior Movimento)';
+	return false;
+}
+
+
+
+
+
+
     }
-    
-    
 }
